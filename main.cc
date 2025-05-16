f9b855e27b2996e9152298e557172bad99fb687c
Bartimaeus812 <95657687+Bartimaeus812@users.noreply.github.com>
2025-05-15 23:40:21 -0400

    Added network reload
    Now can choose to reload network and save to file or read from saved files

--- main.cc
+++ main.cc
@@ -4,14 +4,25 @@
 using namespace std;
 
 int main() {
-    string obsidian_path = "C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\Thaumcraft";
-    Obsidian ob(obsidian_path, false);
-    Network nodes(ob.getNodes());
-    FindCommons fc(nodes);
-//    fc.calculateDistances();
-    fc.inputHistory("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\history.txt");
     string input0, input1;
     int option0 = 1, option1;
+    cout << "0: Load from file\n1: Reload and save\n? ";
+    cin >> option1;
+    Obsidian* ob;
+    if (option1==1) {
+        ob = new Obsidian("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\Thaumcraft", false);
+        ob->fileOutput("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\network.txt");
+    } else {
+        ob = new Obsidian("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\network.txt", true);
+    }
+    Network nodes(ob->getNodes());
+    FindCommons fc(nodes);
+    if (option1==1) {
+        fc.calculateDistances();
+        fc.outputHistory("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\history.txt");
+    } else {
+        fc.inputHistory("C:\\Users\\Danny\\Documents\\GitHub\\Thaumcraft-Solver\\history.txt");
+    }
     cout << "Forms:\n0: exits\n1 number_of_nodes node0 node1 ...: finds potential common nodes\n2 nodeA nodeB: lists fastest paths between them\n";
     cin.clear();
     while (option0>0) {
